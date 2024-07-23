03b58f96aacc07852cd2393949e2a511695b1171
1Seven3Seven <72650065+1Seven3Seven@users.noreply.github.com>
2024-07-24 02:57:31 +0930

    Impoting the potions in the game file package

--- GameFiles/__init__.py
+++ GameFiles/__init__.py
@@ -1,3 +1,5 @@
+from . import Helpers
+
 from .Entity import Entity
 
 from .LightSource import LightSource
@@ -5,6 +7,9 @@ from .Shadows import Shadows
 
 from .MapData import MapData
 from .Map import Map
+
+from .Potion import Potion
+from .PotionHandler import PotionHandler
 
 from .Player import Player
 
