e5eddd3c3628b8d70c42312fbe40e5a4f5c44e5e
1Seven3Seven <72650065+1Seven3Seven@users.noreply.github.com>
2024-07-24 02:56:06 +0930

    Classes to create and handle potions to be thrown by the player

---
+++ GameFiles/Potion.py
@@ -0,0 +1,67 @@
+import math
+
+import pygame
+
+from .LightSource import LightSource
+from .Map import Map
+from .Shadows import Shadows
+
+
+class Potion:
+    brightness: int = 300
+    radius: int = 128
+
+    def __init__(self, x: int | float, y: int | float, angle: float, velocity: int | float):
+        """
+        :param x: The initial x position of the potion.
+        :param y: The initial y position of the potion.
+        :param angle: The angle the potion is thrown at in radians.
+        :param velocity: The velocity the position is thrown at.
+        """
+
+        self.x = x
+        self.y = y
+
+        self.angle = angle
+        self.velocity = velocity
+
+        self.vx = math.cos(angle)
+        self.vy = math.sin(angle)
+
+        self.exploded: bool = False
+        """If true, then this potion has exploded and should be removed."""
+
+    def explode(self, shadows: Shadows) -> None:
+        self.exploded = True
+
+        shadows.add_light_source(
+            LightSource(self.x, self.y, self.brightness, self.radius)
+        )
+
+    def update(self, map_: Map, enemies: list, shadows: Shadows) -> None:
+        # ToDo: add collision with enemies
+
+        # Should not occur, but if the potion has collided with something, then do not update
+        if self.exploded:
+            return
+
+        # If no longer moving
+        if self.velocity == 0:
+            self.explode(shadows)
+            return
+
+        # If moving, then move
+        self.x += self.vx * self.velocity
+        self.y += self.vy * self.velocity
+        self.velocity -= 1
+
+        # Check for collisions with tiles
+        for rect in map_.surrounding_tiles(self.x, self.y):
+            if rect.collidepoint(self.x, self.y):
+                self.explode(shadows)
+
+    def draw(self, screen: pygame.Surface) -> None:
+        # ToDo: draw the potion
+        # A glowing circle will be good enough
+
+        pygame.draw.circle(screen, (0, 255, 0), (self.x, self.y), 10)
---
+++ GameFiles/PotionHandler.py
@@ -0,0 +1,28 @@
+from typing import Generator
+
+import pygame
+
+from .Map import Map
+from .Potion import Potion
+from .Shadows import Shadows
+
+
+class PotionHandler:
+    def __init__(self):
+        self.potions: list[Potion] = []
+
+    def iter_potions_reverse(self) -> Generator[tuple[int, Potion], None, None]:
+        for i in range(len(self.potions) - 1, -1, -1):
+            yield i, self.potions[i]
+
+    def update(self, map_: Map, enemies: list, shadows: Shadows) -> None:
+        for i, potion in self.iter_potions_reverse():
+            if potion.exploded:
+                del self.potions[i]
+                continue
+
+            potion.update(map_, enemies, shadows)
+
+    def draw(self, surface: pygame.Surface) -> None:
+        for potion in self.potions:
+            potion.draw(surface)
