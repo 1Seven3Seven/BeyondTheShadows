61d7e186ac290954d880df4bb11c4870c8255289
1Seven3Seven <72650065+1Seven3Seven@users.noreply.github.com>
2024-07-28 01:45:18 +0930

    Also forgot to add this, gave the potion a crappy animation. I think I want to just place a simple sprite rather than this

--- GameFiles/PotionExploded.py
+++ GameFiles/PotionExploded.py
@@ -14,9 +14,10 @@ class PotionExploded:
     BRIGHTNESS: int = 300
     LIGHT_RADIUS: int = 128
 
-    SIZE: int = 6
+    SIZE: int = 20
     """The size of the collision rectangle. Should be even."""
     SIZE_2: int = int(SIZE // 2)
+    SIZE_4: int = int(SIZE // 4)
 
     PARTICLE_SPAWN_TIME: int = 5
     """The delay in frames between spawning particles."""
@@ -33,27 +34,39 @@ class PotionExploded:
         )
         shadows.add_light_source(self.light_source)
 
-        self.timer: int = self.PARTICLE_SPAWN_TIME
+        self.particle_spawn_timer: int = self.PARTICLE_SPAWN_TIME
+        self.timer: int = 0
 
     def update(self, particle_handler: ParticleHandler) -> None:
-        if self.timer >= 0:
-            self.timer -= 1
+        self.timer += 1
+
+        if self.particle_spawn_timer >= 0:
+            self.particle_spawn_timer -= 1
             return
 
-        self.timer = self.PARTICLE_SPAWN_TIME
+        self.particle_spawn_timer = self.PARTICLE_SPAWN_TIME
 
         angle = random.uniform(0, 2 * math.pi)
-        vx = math.cos(angle)
-        vy = math.sin(angle)
+        vx = math.cos(angle) / 10
+        vy = math.sin(angle) / 10
 
         particle_handler.create_particle(
-            "test",
+            "exploded potion",
             self.x, self.y,
             vx, vy,
             self.PARTICLE_LIFESPAN
         )
 
     def draw(self, camera: Camera) -> None:
+        if not camera.can_see(self.rect):
+            return
+
         display_coords = camera.coordinates_to_display_coordinates(self.rect.center)
 
-        pygame.draw.circle(camera.window, (255, 127, 0), display_coords, 10)
+        radius = self.SIZE_4 + self.SIZE_4 * (1 + math.cos(self.timer / 250 * math.pi * 2)) / 2
+
+        pygame.draw.circle(camera.window, (255, 127, 0), display_coords, radius)
+
+        display_rect = self.rect.copy()
+        camera.convert_rect_to_camera_coordinates(display_rect)
+        pygame.draw.rect(camera.window, (255, 255, 255), display_rect, 1)
