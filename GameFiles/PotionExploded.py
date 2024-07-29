ef6be33197b0a3c508906debee9fd03c26bd9f30
1Seven3Seven <72650065+1Seven3Seven@users.noreply.github.com>
2024-07-29 21:29:17 +0930

    The exploded potions now wear out and deal damage over time to nearby enemies.

--- GameFiles/PotionExploded.py
+++ GameFiles/PotionExploded.py
@@ -4,6 +4,7 @@ import random
 import pygame
 
 from .Camera import Camera
+from .Entity import Entity
 from .LightSource import LightSource
 from .ParticleHandler import ParticleHandler
 from .Shadows import Shadows
@@ -19,12 +20,33 @@ class PotionExploded:
     """The delay in frames between spawning particles."""
     PARTICLE_LIFESPAN: int = 20
 
+    HEALTH: int = 100  # 100 as base and 200 as max
+    """
+    The 'health' of the potion.
+    An exploded potions 'health' gets drained when it causes damage to enemies.
+    """
+
+    DAMAGE: int = 1
+    """
+    The amount of damage done every damage frame.
+    """
+
+    DAMAGE_FRAME: int = 6
+
+    WEAR_OUT_FRAME: int = 18  # Lasts 30 seconds if nothing happens and 60 at max health
+
+    # Note the max damage delt via exposure is HEALTH * DAMAGE
+
     @classmethod
     def increase_light_radius(cls, increase_by: int = 16) -> None:
         cls.LIGHT_RADIUS += increase_by
         cls.LIGHT_RADIUS_SQRT_2 = cls.LIGHT_RADIUS * math.sqrt(2)
 
-    def __init__(self, x: int | float, y: int | float, shadows: Shadows):
+    @classmethod
+    def increase_health(cls, increase_by: int = 50) -> None:
+        cls.HEALTH += increase_by
+
+    def __init__(self, x: int | float, y: int | float, shadows: Shadows, update_offset: int = 0):
         self.x = x
         self.y = y
 
@@ -37,11 +59,18 @@ class PotionExploded:
         shadows.add_light_source(self.light_source)
 
         self.particle_spawn_timer: int = self.PARTICLE_SPAWN_TIME
-        self.timer: int = 0
 
-    def update(self, particle_handler: ParticleHandler) -> None:
-        self.timer += 1
+        self.health: int = self.HEALTH
 
+        self.damage: int = self.DAMAGE
+        self.damage_timer: int = 0 + update_offset
+
+        self.wear_out_timer: int = self.WEAR_OUT_FRAME
+
+        self.double_exploded: bool = False
+        """If double exploded, then this exploded potion should be removed."""
+
+    def _spawn_particles(self, particle_handler: ParticleHandler) -> None:
         if self.particle_spawn_timer >= 0:
             self.particle_spawn_timer -= 1
             return
@@ -58,6 +87,65 @@ class PotionExploded:
             vx, vy,
             self.PARTICLE_LIFESPAN
         )
+
+    def _damage_enemies(self, particle_handler: ParticleHandler, enemies: list[Entity]) -> bool:
+        """
+        Returns true of the potion has double exploded.
+        """
+
+        if self.damage_timer >= 0:
+            self.damage_timer -= 1
+            return False
+
+        self.damage_timer = self.DAMAGE_FRAME
+
+        for enemy in enemies:
+            if self.rect.colliderect(enemy.rect):
+                enemy.deal_damage(self.damage)
+                self.health -= 1
+
+                print(f"Health: {self.health}")
+
+                if self.health <= 0:
+                    self._double_explode(particle_handler)
+                    return True
+
+        return False
+
+    def _double_explode(self, particle_handler: ParticleHandler) -> None:
+        # ToDo: explode with style, and particles
+        self.double_exploded = True
+
+    def _wear_out(self) -> bool:
+        """
+        Returns true if the potion has worn out.
+        """
+
+        if self.wear_out_timer >= 0:
+            self.wear_out_timer -= 1
+            return False
+
+        self.wear_out_timer = self.WEAR_OUT_FRAME
+
+        self.health -= 1
+
+        if self.health <= 0:
+            self.double_exploded = True
+            return True
+        return False
+
+    def update(self, particle_handler: ParticleHandler, enemies: list[Entity]) -> None:
+        # Should not occur, do thing if to be deleted
+        if self.double_exploded:
+            return
+
+        if self._damage_enemies(particle_handler, enemies):
+            return
+
+        if self._wear_out():
+            return
+
+        self._spawn_particles(particle_handler)
 
     def draw(self, camera: Camera) -> None:
         if not camera.can_see(self.rect):
